ea1e70c0d168a55546012cc937b9d8b48f4d47a5
Tsenkovp <tsenkovp@gmail.com>
2026-03-27 20:25:27 +0200

    add DAL

---
+++ CarMVC25.Data/Entities/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarMVC25.Data.Entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+        public string ModelName { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public string Fuel { get; set; }
+        public string ImageUrl { get; set; }
+    }
+
+}
